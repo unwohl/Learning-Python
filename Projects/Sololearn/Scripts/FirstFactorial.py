3678ebdabb4856eb0e0f7254ccc1a8481261d114
Dawinartor <david.melamed@web.de>
2019-03-07 14:47:33 +0100

    Test some thing

---
+++ Projects/Sololearn/Scripts/FirstFactorial.py
@@ -0,0 +1,9 @@
+def Fakultät(num):
+    for i in range(num + 1):
+            if i < 0:
+                i = 1
+        else:
+                        i = i
+                        print(i)
+
+Fakultät(4)
