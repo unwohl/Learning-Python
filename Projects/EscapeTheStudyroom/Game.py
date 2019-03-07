f35fca056488a80dcd9974b7e28b8d6faa1dc91e
Altanoras <jokaphi@gmx.de>
2019-03-07 16:59:52 +0100

    Everything works!

--- Projects/EscapeTheStudyroom/Game.py
+++ Projects/EscapeTheStudyroom/Game.py
@@ -1,1 +1,14 @@
+from Thing import Thing
+from Room import Room
 import Parser
+
+game_ended = False
+
+inventory = {}
+
+roomID = 0
+
+Rooms = open("Rooms.txt","r")
+
+while not game_ended:
+    _Input = Parser.getInput()
--- Projects/EscapeTheStudyroom/Parser.py
+++ Projects/EscapeTheStudyroom/Parser.py
@@ -1,16 +1,14 @@
 pattern = ("examine","use","take","combine")
 
 abort = "Wrong input!"
-abort2 = "Too many words in input!"
+abort2 = "Too many / not enough words in input!"
 def getInput():
     _input = input(":")
     inputs = _input.split(" ")
-    print(inputs)
     if not inputs[0] in pattern:
         print(abort)
         if len(inputs) > 3 or len(inputs) < 2:
             print(abort2)
         return
     return inputs
-    
-getInput()
+
