40973315a92785a6b4d42a821ac061c97fcc97d0
Altanoras <jokaphi@gmx.de>
2019-03-07 15:22:12 +0100

    Test

---
+++ Projects/EscapeTheStudyroom/Game.py
@@ -0,0 +1,7 @@
+import Thing
+
+Desk = Thing(1,"Dies ist ein Schreibtisch")
+Chair = Thing(2,"Dies ist ein Stuhl.")
+
+print(Chair.description)
+print(Desk.ID)
---
+++ Projects/EscapeTheStudyroom/Thing.py
@@ -0,0 +1,13 @@
+class Thing:
+    descripton = "No description available..."
+    ID = 0
+
+    def __init__(self,arg1,arg2):
+        self.description = arg1
+        self.ID = arg2 
+
+    def examine():
+        return description
+
+
+    
