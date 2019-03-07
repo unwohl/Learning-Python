2f30ffb3fc95e64d54d818c7ad2333fea66cee38
Altanoras <jokaphi@gmx.de>
2019-03-07 15:41:52 +0100

    Yes

--- Projects/EscapeTheStudyroom/Game.py
+++ Projects/EscapeTheStudyroom/Game.py
@@ -1,7 +1,1 @@
 from Thing import Thing
-
-desk = Thing("Dies ist ein Schreibtisch",1)
-empty = Thing()
-
-print(empty.examine())
-print(desk.getID())
--- Projects/EscapeTheStudyroom/Thing.py
+++ Projects/EscapeTheStudyroom/Thing.py
@@ -1,16 +1,12 @@
 class Thing:
-    def __init__(self):
-        self.description = "No description available..."
-        self.ID = 0
-
-    def __init__(self,arg1,arg2):
+    def __init__(self,arg1 = "No description available...",arg2 = 0):
         self.description = arg1
         self.ID = arg2 
 
-    def examine():
-        return self.description
+    def examine(self):
+        print(self.description)
 
-    def getID():
+    def getID(self):
         return self.ID
 
 
