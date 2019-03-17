eb47456a40244bccabc3a0313642177bf1e1856e
Altanoras <jokaphi@gmx.de>
2019-03-17 20:00:32 +0100

    GUBIAWIUBRFUIW

--- Projects/JoshgasIntelliJ/src/Items/Thing.java
+++ Projects/JoshgasIntelliJ/src/Items/Thing.java
@@ -1,7 +1,27 @@
 package Items;
 
 public class Thing {
-    public Thing(String arg1, int arg2){
-        
+    int ThingCount = 0;
+
+    String _description = "No description available...";
+    int _ID = 0;
+
+    public Thing(String arg1){ //arg1 = description
+        _description = arg1;
+        ThingCount++;
+        _ID = ThingCount;
+    }
+
+    public Thing(){
+        ThingCount++;
+        _ID = ThingCount;
+    }
+
+    public String examine(){
+        return _description;
+    }
+
+    public int getID(){
+        return _ID;
     }
 }
